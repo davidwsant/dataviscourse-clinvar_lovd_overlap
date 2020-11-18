8c280383c134f2b47a64d4791cc1bf598cc6d232
Dallon Durfey <dallondurfey@gmail.com>
2020-11-17 22:55:19 -0700

    read data and make arrays of genes and diseases

---
+++ js/script.js
@@ -0,0 +1,40 @@
+// read the csv using d3-fetch
+
+d3.csv('SQL/All_Invalid_SQL_Input.csv').then(function(data){
+    console.log(data)
+
+    // call main class
+    new Main(data)
+});
+
+
+class Main {
+
+    constructor(data) {
+        
+
+        // get all unique diseases and genes from the data
+        this.diseases = []
+        this.genes = []
+
+        for (let d of data){
+            let disease = d.Disease
+            let gene = d["Gene Symbol"]
+            if (!this.diseases.includes(disease)){
+                this.diseases.push(disease)
+            }
+            if (!this.genes.includes(gene)){
+                this.genes.push(gene)
+            }
+        }
+
+        console.log(this.diseases)
+        console.log(this.genes)
+
+
+
+
+    }
+
+
+}
