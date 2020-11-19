d5fe25a95b38b5d2aa46f71dc1754be5c6bb85d0
David Sant <david.sant@utah.edu>
2020-11-19 13:46:03 -0700

    Added filters object into constructor of main

--- js/script.js
+++ js/script.js
@@ -1,10 +1,12 @@
 // read the csv using d3-fetch
 
 d3.csv('data/MLD_Valid.csv').then(function(data){
-    console.log(data)
+    //console.log(data)
 
     // call main class
     new Main(data)
+    
+    
 });
 
 
@@ -27,13 +29,128 @@ class Main {
             }
         }
 
-        console.log(this.diseases)
-        console.log(this.genes)
-
-
+        this.filters = {'databases': {
+            'ClinVar': true,
+            'Human_Variome': true,
+            'Global_Variome': true,
+            'BIPmed_WES': true,
+            'BIPmed_SNPhg19': true,
+            'MSeqDR-LSDB': true,
+            'CCHMC': true
+        },
+        'varType': {
+            'single nucleotide variant': true,
+            'Indel': true,
+            'Deletion': true,
+            'Duplication': true,
+            'Insertion': true
+        },
+        'pathogenicity': {
+            'Benign': true,
+            'Likely Benign': true,
+            'VUS': true,
+            'Likely Pathogenic': true,
+            'Pathogenic': true,
+            'Conflicting': true,
+            'Not Provided': true
+        },
+        'reviewStatus': {
+            '0': true,
+            '1': true,
+            '2': true
+        }}
+        
+        this.eventListeners()
+        // console.log(this.diseases)
+        // console.log(this.genes)
 
 
     }
+    eventListeners() {
+        //let clinVarButton = 
+        d3.select('#checkBoxClinVar').on('change', (event) => {
+            this.filters['databases']['ClinVar'] = event.path[0].checked
+            console.log(this.filters)
+        })
+        d3.select('#checkBoxGlob').on('change', (event) => {
+            this.filters['databases']['Global_Variome'] = event.path[0].checked
+        })
+        d3.select('#checkBoxHum').on('change', (event) => {
+            this.filters['databases']['Human_Variome'] = event.path[0].checked
+        })
+        d3.select('#checkBoxBIPSNP').on('change', (event) => {
+            this.filters['databases']['BIPmed_SNPhg19'] = event.path[0].checked
+        })
+        d3.select('#checkBoxBIPWES').on('change', (event) => {
+            this.filters['databases']['BIPmed_WES'] = event.path[0].checked
+        })
+        d3.select('#checkBoxMSEQ').on('change', (event) => {
+            this.filters['databases']['MSeqDR-LSDB'] = event.path[0].checked
+        })
+        d3.select('#checkBoxCCHMC').on('change', (event) => {
+            this.filters['databases']['CCHMC'] = event.path[0].checked
+        })
+
+        // This was the way to use the DOM instead of d3 
+        // let globButton = document.getElementById('checkBoxGlob')
+        // globButton.addEventListener('change', (event) => {
+        //     this.filters['databases']['Global_Variome'] = event.target.checked
+        // })
+
+        // Now for variant type
+        d3.select('#checkBoxSNV').on('change', (event) => {
+            this.filters['varType']['single nucleotide variant'] = event.path[0].checked
+        })
+        d3.select('#checkBoxDel').on('change', (event) => {
+            this.filters['varType']['Deletion'] = event.path[0].checked
+        })
+        d3.select('#checkBoxDup').on('change', (event) => {
+            this.filters['varType']['Duplication'] = event.path[0].checked
+        })
+        d3.select('#checkBoxIns').on('change', (event) => {
+            this.filters['varType']['Insertion'] = event.path[0].checked
+        })
+        d3.select('#checkBoxIndel').on('change', (event) => {
+            this.filters['varType']['Indel'] = event.path[0].checked
+        })
+
+        // Now for pathogenicity
+        d3.select('#checkBoxBenign').on('change', (event) => {
+            this.filters['pathogenicity']['Benign'] = event.path[0].checked
+        })
+        d3.select('#checkBoxLikBenign').on('change', (event) => {
+            this.filters['pathogenicity']['Likely Benign'] = event.path[0].checked
+        })
+        d3.select('#checkBoxVUS').on('change', (event) => {
+            this.filters['pathogenicity']['VUS'] = event.path[0].checked
+        })
+        d3.select('#checkBoxLikPath').on('change', (event) => {
+            this.filters['pathogenicity']['Likely Pathogenic'] = event.path[0].checked
+        })
+        d3.select('#checkBoxPath').on('change', (event) => {
+            this.filters['pathogenicity']['Pathogenic'] = event.path[0].checked
+        })
+        d3.select('#checkBoxCon').on('change', (event) => {
+            this.filters['pathogenicity']['Conflicting'] = event.path[0].checked
+        })
+        d3.select('#checkBoxNotPro').on('change', (event) => {
+            this.filters['pathogenicity']['Not Provided'] = event.path[0].checked
+        })
+
+        // Now for Review Status
+        d3.select('#checkBoxReview0').on('change', (event) => {
+            this.filters['reviewStatus']['0'] = event.path[0].checked
+        })
+        d3.select('#checkBoxReview1').on('change', (event) => {
+            this.filters['reviewStatus']['1'] = event.path[0].checked
+        })
+        d3.select('#checkBoxReview2').on('change', (event) => {
+            this.filters['reviewStatus']['2'] = event.path[0].checked
+        })
+
+
+    }
+    
 
 
 }
