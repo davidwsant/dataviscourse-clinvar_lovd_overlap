7b00caf3d7e085e3ed21ef8fd250bf981bf91920
Dallon Durfey <dallondurfey@gmail.com>
2020-11-20 15:20:00 -0700

    axes and data finished

--- js/script.js
+++ js/script.js
@@ -1,5 +1,4 @@
 // read the csv using d3-fetch
-
 Promise.all([
     d3.csv("data/MLD_Valid.csv"), // read in the MLD data
     d3.json("data/Genomic_Features.json"), // read the genomic features data
@@ -23,8 +22,8 @@ class Main {
         this.invalid_data = invalid_data
 
         console.log(MLD_data)
-        console.log(genomic_features)
-        console.log(invalid_data)
+        // console.log(genomic_features)
+        // console.log(invalid_data)
 
         // get all unique diseases and genes from the data
         this.diseases = []
@@ -73,10 +72,10 @@ class Main {
         }}
         
         this.eventListeners()
+        this.pathogenicityGraph()
         // console.log(this.diseases)
         // console.log(this.genes)
-        // this.drawItNow()
-        // this.redraw()
+ 
 
 
     }
@@ -84,7 +83,7 @@ class Main {
         //let clinVarButton = 
         d3.select('#checkBoxClinVar').on('change', (event) => {
             this.filters['databases']['ClinVar'] = event.path[0].checked
-            console.log(this.filters)
+            // console.log(this.filters)
         })
         d3.select('#checkBoxGlob').on('change', (event) => {
             this.filters['databases']['Global_Variome'] = event.path[0].checked
@@ -161,6 +160,190 @@ class Main {
         d3.select('#checkBoxReview2').on('change', (event) => {
             this.filters['reviewStatus']['2'] = event.path[0].checked
         })
+        
+
+
+    }
+
+    pathogenicityGraph(){
+
+        // first set up the data 
+        let pathogenicityClasses = ['Benign', 'Likely Benign', 'VUS', 
+        'Likely Pathogenic', 'Pathogenic', 'Conflicting', 'Not Provided']
+
+        let gene_symbols = ['ARSA', 'PSAP', 'SUMF1']
+
+        // only include pathogenicity classes that are selected in the page
+        let includedClasses = []
+
+        for (let i of Object.entries(this.filters.pathogenicity)){
+            if (i[1]){
+                includedClasses.push(i[0])
+            }
+        };
+
+        let benign_list = []
+        let likely_benign_list = []
+        let vus_list = []
+        let likely_pathogenic_list = []
+        let pathogenic_list = []
+        let conflicting_list = []
+        let not_provided_list = []
+
+        // loop through the MLD_data to get all the pathogenicity values
+        for (let i of this.MLD_data) {
+            let pathogenicity = i.Pathogenicity
+            let hgvs = i['HGVS Normalized Genomic Annotation']
+            let gene_symbol = i['Gene Symbol']
+
+            // check that the pathogenicity is selected on the page
+            if (includedClasses.includes(pathogenicity)){
+
+                // now we want to check which pathogenicity it is, and make sure the 
+                // HGVS normalized genomic annotation isn't already in the list to avoid duplicates
+                if (pathogenicity === 'Benign' && !benign_list.includes(hgvs)){
+                    benign_list.push({'HGVS': hgvs, 'gene_symbol': gene_symbol})
+                }
+
+                else if (pathogenicity === 'Likely Benign' && !likely_benign_list.includes(hgvs)){
+                    likely_benign_list.push({'HGVS': hgvs, 'gene_symbol': gene_symbol})
+                }
+
+                else if (pathogenicity === 'VUS' && !vus_list.includes(hgvs)){
+                    vus_list.push({'HGVS': hgvs, 'gene_symbol': gene_symbol})
+                }
+
+                else if (pathogenicity === 'Likely Pathogenic' && 
+                !likely_pathogenic_list.includes(hgvs)){
+                    likely_pathogenic_list.push({'HGVS': hgvs, 'gene_symbol': gene_symbol})
+                }
+
+                else if (pathogenicity === 'Pathogenic' && !pathogenic_list.includes(hgvs)){
+                    pathogenic_list.push({'HGVS': hgvs, 'gene_symbol': gene_symbol})
+                }
+            
+                else if (pathogenicity === 'Conflicting' && !conflicting_list.includes(hgvs)){
+                    conflicting_list.push({'HGVS': hgvs, 'gene_symbol': gene_symbol})
+                }
+
+                else if (pathogenicity === 'Not Provided' && !not_provided_list.includes(hgvs)){
+                    not_provided_list.push({'HGVS': hgvs, 'gene_symbol': gene_symbol})
+                }
+
+            }
+            
+        }
+
+        // now, determine the total count of all the pathogenicity classes
+
+        let all_lists = [benign_list, likely_benign_list, vus_list, 
+            likely_pathogenic_list, pathogenic_list, conflicting_list, 
+            not_provided_list]
+
+        let total_count = 0
+        let total_arsa = 0
+        let total_psap = 0
+        let total_sumf1 = 0
+
+        // list of counts for ARSA, PSAP, and SUMF1 genes in that order
+        let list_of_counts = [new Array(7).fill(0), new Array(7).fill(0), new Array(7).fill(0)]
+        
+        
+        for (let i = 0; i<all_lists.length; i++){
+            let list = all_lists[i]
+            total_count = total_count += list.length
+            for (let j = 0; j<list.length; j++){
+                let item = list[j]
+                // let gene_symbol = item.gene_symbol
+                if (item.gene_symbol === 'ARSA'){
+                    total_arsa ++
+                    list_of_counts[0][i] ++
+                }
+                else if (item.gene_symbol === 'PSAP'){
+                    total_psap ++
+                    list_of_counts[1][i] ++
+                }
+                else if (item.gene_symbol === 'SUMF1'){
+                    total_sumf1 ++
+                    list_of_counts[2][i] ++
+                }
+            }
+        }
+
+        let arsa_percentages = list_of_counts[0].map(function(x){return x/total_arsa})
+        let psap_percentages = list_of_counts[1].map(function(x){return x/total_psap})
+        let sumf1_percentages = list_of_counts[2].map(function(x){return x/total_sumf1})
+
+
+        let percentages = [arsa_percentages, psap_percentages, sumf1_percentages]
+
+        let percentages_data = []
+        for (let i=0; i<percentages.length; i++){
+            let symbol = gene_symbols[i]
+            let list = percentages[i]
+            let temp_data = []
+            for (let j=0; j<arsa_percentages.length; j++){
+                temp_data.push({'gene': symbol, 'pathogenicity': pathogenicityClasses[j], 
+                'value': list[j]})
+            }
+            percentages_data.push(temp_data)
+        }
+        
+        console.log(percentages_data)
+
+        // set up the svg
+        let svg_width = 600
+        let svg_height = 400
+        let margins = 30
+
+        let pathogenicityGraphSVG = d3.select('#pathogenicity-graph').append('svg')
+            .attr('width', svg_width)
+            .attr('height', svg_height)
+            .attr('id', 'pathogenicity-svg')
+
+        // define scales
+        let scaleX = d3.scaleBand()
+            .domain(pathogenicityClasses)
+            .range([0, svg_width - margins])
+
+
+        let scaleY = d3.scaleLinear()
+            .domain([1, 0])
+            .range([0, svg_height])
+
+        // set up axes
+        let y_axis = d3.axisLeft(scaleY)
+            .ticks(4)
+
+        let colorScale = d3.scaleOrdinal()
+            .domain(pathogenicityClasses)
+            .range(['red', 'blue', 'green'])
+
+        
+        let x_axis = d3.axisBottom(scaleX)
+            .ticks(8)
+            .tickFormat((d,i)=>pathogenicityClasses[i])
+
+        
+        let y_axis_group = pathogenicityGraphSVG.append('g')
+            .attr('id', 'pathogenicity-y-axis')
+            .call(y_axis)
+            .attr('transform', `translate(${margins}, ${-margins})`)
+        
+        let x_axis_group = pathogenicityGraphSVG.append('g')
+            .attr('id', 'pathogenicity-x-axis')
+            .call(x_axis)
+            .attr('transform', `translate(${margins}, ${svg_height - margins})`)
+        
+        // draw the graph
+
+        let linesgenerator = d3.line()
+            .x(d=>d.pathogenicity)
+            .y(d=>d.value)
+            
+
+
+
 
 
     }
